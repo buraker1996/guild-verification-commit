9b348dd8b4d780fdd6220c569a4c9a4863766554
buraker1996 <166491383+buraker1996@users.noreply.github.com>
2026-03-09 12:39:04 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T09:39:02.130Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-09T09:39:04.875Z
+Commit: Fix CSS styling bug on mobile devices
