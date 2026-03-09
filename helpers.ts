99191130efe8549a4fe5d64a72e2afc27f91fb8d
buraker1996 <166491383+buraker1996@users.noreply.github.com>
2026-03-09 14:22:09 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T11:22:08.550Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-09T11:22:09.703Z
+Commit: Fix memory leak in event handlers
